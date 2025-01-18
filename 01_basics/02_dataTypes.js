2e3ffa0c5dacf4aac45db63021a75467794c5b51
Muhammad-Mohsin0 <154534482+Muhammad-Mohsin0@users.noreply.github.com>
2025-01-18 12:04:13 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict"; // treat all js code as newer version
+
+//alert(3 + 3) // we are using node.js not in browser
+
+console.log( 3 
+    
+    + 3); // code readability should be high
+
+console.log("Mosi");
+
+
+let name ="Mohsin"
+let age = 25
+let isLoggedIn= true
+
+// number => 2 to power 53 
+//bigInt 
+// string => "Mohsin"
+//boolean => true/false
+//null => standalone value
+//undefined => values is not assigned
+//symbol => unique
+
+
+//object 
+
+console.log(typeof undefined); // undefined
+console.log(typeof null);      // object
+
+    
