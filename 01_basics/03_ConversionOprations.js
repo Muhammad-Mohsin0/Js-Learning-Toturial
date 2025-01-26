9870bd4f68cbe0a6a181293419faa869b69b6f43
Muhammad-Mohsin0 <154534482+Muhammad-Mohsin0@users.noreply.github.com>
2025-01-26 13:06:26 +0000

    Conversion Of Datatypes

---
+++ 01_basics/03_ConversionOprations.js
@@ -0,0 +1,31 @@
+let score = "33ew"
+let name = "mohsin"
+
+//console.log(typeof score);
+//console.log(typeof (score));
+//console.log(typeof (name));
+
+let vlaueInNumber= Number(score)
+//console.log(typeof vlaueInNumber);
+//console.log(vlaueInNumber);
+
+// "33" => 33
+// "33ew" => NaN
+// true => 1 ,false => 0
+
+let isLoggedIn = "mohsin"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1  =>  true, 0 => false
+// "" => false
+// "mohsin" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
