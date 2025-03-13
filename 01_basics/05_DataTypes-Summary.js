1f0e08387b8147b8cea5b0e729b2b1e585e9a864
Muhammad-Mohsin0 <154534482+Muhammad-Mohsin0@users.noreply.github.com>
2025-03-13 07:26:48 +0000

    datatypes summary

---
+++ 01_basics/05_DataTypes-Summary.js
@@ -0,0 +1,38 @@
+// # Primitive DataTypes
+
+// 7 Types: String, Number, Boolean, Null, Undefined, Symbol, BigInt
+
+const score =100
+const scoreValue=100.3
+
+const isLoggedIn=false
+const outSideTemp =null
+
+let userEmail;
+
+const id=Symbol('123')
+const anotherId=Symbol('123')
+
+// console.log(id === anotherId);
+
+// const bigNumber= 43543252525235353n  
+
+// Reference (Non-Primitive DataTypes)
+
+// Array, Objects, Functions
+
+const heros = ["Imran khan", "Qaud","Iqbal"]
+let myObj = {
+    name:"mosi",
+    age: 25,
+}
+
+const myFunction= function(){
+    console.log("Helloo World")
+}
+
+console.log(typeof bigNumber);
+
+
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
