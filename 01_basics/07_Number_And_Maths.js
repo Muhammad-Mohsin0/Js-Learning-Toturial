ccd73f81a643065a10580619dfdf85c331b0b3d9
Muhammad-Mohsin0 <154534482+Muhammad-Mohsin0@users.noreply.github.com>
2025-03-15 05:38:18 +0000

    Nums Nd Math

---
+++ 01_basics/07_Number_And_Maths.js
@@ -0,0 +1,35 @@
+// +++++++++++++++++++++ Numbers ++++++++++++++++
+
+const score =400
+
+const balance = new Number(100)
+// console.log(balance);
+// console.log(balance.toString());
+// console.log(balance.toFixed(2));
+
+const otherNumber = 123.8999
+// console.log(otherNumber.toPrecision(4));
+
+const hundreds= 10000000
+// console.log(hundreds.toLocaleString('en-IN'));
+// console.log(hundreds.toLocaleString());
+
+//+++++++++++++++++++++ Maths ++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.PI);
+// console.log(Math.abs(-5));
+// console.log(Math.round(5.4));
+// console.log(Math.ceil(5.1));
+// console.log(Math.floor(5.9));
+
+// console.log(Math.min(5.9,49,6,7,7,7));
+// console.log(Math.max(5.9,49,6,7,7,7));
+
+// console.log(Math.random());
+// console.log((Math.random()*10)+1);
+// console.log(Math.floor(Math.random()*10)+1);
+
+const min=10
+const max=20
+console.log(Math.floor(Math.random()*(max-min+1)+min))
