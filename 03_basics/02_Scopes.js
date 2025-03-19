7dfd75261652e9c4bfd3c489372e0c4009ee28b2
Muhammad-Mohsin0 <154534482+Muhammad-Mohsin0@users.noreply.github.com>
2025-03-19 10:49:11 +0000

    Global & Block Scop

---
+++ 03_basics/02_Scopes.js
@@ -0,0 +1,14 @@
+//Scopes, Global Scope and Block Scope
+
+// var c=300
+let a =2333
+if(true){
+    let a= 10
+    const b=20
+    var c=
+    console.log(`INNER: ${a}`);
+    
+}
+console.log(a);
+// console.log(b);
+// console.log(c);
