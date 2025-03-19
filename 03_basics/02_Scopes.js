dbffdc928291c4ea82b8a952fee0a7d2ba403a4c
Muhammad-Mohsin0 <154534482+Muhammad-Mohsin0@users.noreply.github.com>
2025-03-19 14:58:57 +0000

    Scopes and Hoisting

--- 03_basics/02_Scopes.js
+++ 03_basics/02_Scopes.js
@@ -5,10 +5,49 @@ let a =2333
 if(true){
     let a= 10
     const b=20
-    var c=
-    console.log(`INNER: ${a}`);
-    
+    var c=3
+    // console.log(`INNER: ${a}`);
 }
-console.log(a);
+
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+
+//++++++++++++++ Nested Scope
+function one(){
+    const username="Mohsin"
+    function two(){
+        const website="Youtube"
+        // console.log(username);
+    }
+    //console.log(website)
+    two()
+}
+one()
+//Clousre ?
+
+
+if(true){
+    const username="moshina"
+    if(username==="moshina"){
+        const website=" Youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+// console.log(username);
+
+
+//++++++++ Interesting++++++++++
+// Hoisting
+
+console.log(addOne(5)); // addOne is accessible before function
+function addOne(num){
+    return num+1
+}
+
+addTwo(5) //addTwo is not accessible before const addTwo 
+const addTwo=function (num){
+    return num +2
+}
