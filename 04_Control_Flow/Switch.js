cbb7baa4adf6055621c85dd54ce8a14f84eaac0d
Muhammad-Mohsin0 <154534482+Muhammad-Mohsin0@users.noreply.github.com>
2025-03-21 11:34:22 +0000

    switch

---
+++ 04_Control_Flow/Switch.js
@@ -0,0 +1,47 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month =2
+switch (month) {
+    case 1:
+        console.log("January");
+        break;
+    case 2:
+        console.log("February");
+        break;
+    case 3:
+        console.log("March");
+        break;
+    case 4:
+        console.log("April");
+        break;
+    default:
+        console.log("Non Of These");
+        break;
+}
+
+//String 
+const months ="march"
+switch (months) {
+    case "jan":
+        console.log("January");
+        break;
+    case "feb":
+        console.log("February");
+        break;
+    case "march":
+        console.log("March");
+        break;
+    case "april":
+        console.log("April");
+        break;
+    default:
+        console.log("Non Of These");
+        break;
+}
