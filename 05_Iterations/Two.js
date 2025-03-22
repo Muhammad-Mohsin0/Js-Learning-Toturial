f188f5e4b318d6a644afdbabd49fb96c133e8cb7
Muhammad-Mohsin0 <154534482+Muhammad-Mohsin0@users.noreply.github.com>
2025-03-22 10:41:01 +0000

    while & do while

---
+++ 05_Iterations/Two.js
@@ -0,0 +1,30 @@
+// while & do while loop
+
+// let number=2
+// while (number <=20) {
+//     console.log(`value of number is ${number}`);
+//     number= number+2
+//     number++
+// }
+
+// let myArray=['batman','superman','padman']
+// let i=0
+// while (i<myArray.length) {
+//     console.log(`here is series of ${myArray[i]}`);
+//     i = i + 1
+// }
+
+// Do while
+
+let myArray=['batman','superman','padman']
+let i=0
+do {
+    // console.log(`here is series of ${myArray[i]}`);
+     i = i + 1
+} while (i<myArray.length);
+
+let score=1
+do {
+    console.log(`the score is ${score}`);
+    score++
+} while (score <=20);
