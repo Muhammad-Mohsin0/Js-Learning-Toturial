b3f13848f373cca359eee64107ce803704d6683e
Muhammad-Mohsin0 <mohsin.signin00@gmail.com>
2025-04-29 23:55:21 -0700

    v8-debug

--- 11_Fun_With_Js/Advance_Array.js
+++ 11_Fun_With_Js/Advance_Array.js
@@ -1,2 +1,24 @@
 const myArray=[]
-DebugPrint(myArray)
+// %DebugPrint(myArray)
+
+
+//continious Array , Holey Array
+
+// SMI (Small Integar)
+// Packed Element
+// Double (float, string, function)
+
+const arrTwo=[1,2,3,4,5]
+// PACKED SMI ELEMENTS
+arrTwo.push(6.0)
+// PACKED DOUBLE ELEMENTS
+arrTwo.push('8')
+// PACKED ELEMENTS
+
+arrTwo[10]=11
+// Holey Elements
+
+console.log(arrTwo);
+console.log(arrTwo.length);
+console.log(arrTwo[9]);
+
