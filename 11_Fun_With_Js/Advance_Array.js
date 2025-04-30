040600e8f2f3eaad17bfe6705b0bb2f02103998b
Muhammad-Mohsin0 <mohsin.signin00@gmail.com>
2025-04-30 00:18:38 -0700

    Array for v8-debug

--- 11_Fun_With_Js/Advance_Array.js
+++ 11_Fun_With_Js/Advance_Array.js
@@ -22,3 +22,29 @@ console.log(arrTwo);
 console.log(arrTwo.length);
 console.log(arrTwo[9]);
 
+// bound check
+// hasOwnProperty(arrTwo, 9)
+// hasOwnProperty(arrTwo.prototype, 10)
+// hasOwnProperty(Object.prototype, 10) // if Holes are then every time all checking is performed. //Holes are very expensive in JS
+
+const arrThree= [1,2,3,4,5]
+console.log(arrThree[2]);
+
+// SMI > DOUBLE > PACKED  // that is optimized  and Contimnious waly
+// H_SMI > H_DOUBLE > H_PACKED   // Holes waly
+
+const arrFour =new Array (3)
+// just 3 holes . HOLEY SMI ELEMENTS
+arrFour[0]='1'  // Holey packed ELEMENTS
+arrFour[1]='2'  // Holey packed ELEMENTS
+arrFour[2]='3'  // Holey packed ELEMENTS
+
+const arrFive = []
+arrFive.push('1') //Packed elements
+arrFive.push('2') //Packed elements
+arrFive.push('3') //Packed elements
+
+const arrSix = [1,2,3,4,5]
+arrSix.push(Infinity)
+
+// for , for-of , forEach
